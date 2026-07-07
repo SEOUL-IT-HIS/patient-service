bab1b2df9f652d3401b71fa3519fe585f4f27bea
DESKTOP-09HGIPC\hhhon <ghddpwls3994@gmail.com>
2026-07-07 11:07:47 +0900

    init

---
+++ src/test/java/kr/co/seoulit/his/patientservice/PatientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.seoulit.his.patientservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
